3559f4421f3a5518b2d0364245d9eee8710727f9
0ambar <abrahamr677@gmail.com>
2026-01-04 13:32:23 -0600

    feat: adding validation with formz and cubits

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/infrastructure/inputs/inputs.dart';
+import 'package:forms_app/presentation/blocs/register_cubit/register_cubit.dart';
 import 'package:forms_app/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -10,7 +13,11 @@ class RegisterScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('New User'),
       ),
-      body: const _RegisterView(),
+      body: BlocProvider(
+        create: (context) => RegisterCubit(),
+        child: const _RegisterView(),
+      )
+      
     );
   }
 }
@@ -45,29 +52,45 @@ class _RegisterForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // This widgets rebuilds when some current change is made
+    final registerCubit = context.watch<RegisterCubit>();
+
+    final Username username = registerCubit.state.username;
+    final Email email = registerCubit.state.email;
+    final Password password = registerCubit.state.password;
+
     return Form(
       child: Column(
         children: [
           CustomTextFormField(
             label: 'Username',
-            hint: 'Jhon Robinson',
+            // hint: 'Jhon Robinson',
+            onChanged: registerCubit.usernameChanged,
+            errorMessage: username.errorMessage
           ),
           const SizedBox(height: 10),
 
           CustomTextFormField(
             label: 'email',
-            hint: 'jhon@email.com',
+            // hint: 'jhon@email.com',
+            onChanged: registerCubit.emailChanged,
+            errorMessage: email.errorMessage
           ),
           const SizedBox(height: 10),
           
           CustomTextFormField(
             label: 'password',
             obscureText: true,
+            onChanged: registerCubit.passwordChanged,
+            errorMessage: password.errorMessage,
           ),
 
           const SizedBox(height: 20),
           FilledButton.tonalIcon(
-            onPressed: () {}, 
+            onPressed: () {
+              registerCubit.onSubmit();
+            }, 
             label: const Text('Save user'),
           ),
         ],
